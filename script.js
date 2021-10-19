dd983ce853ccd90cb447f19fb0385bbb848f5f38
AripovJr <jasurbek_aripov@gmail.ru>
2021-10-19 19:28:11 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const remained =  document.getElementById('remained')
+const liters =  document.getElementById('liters')
+const percentage =  document.getElementById('percentage')
+
+updateBigCup()
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlited(idx))
+})
+
+function highlited(idx){
+    if(smallCups[idx].classList.contains('full') && 
+    !smallCups[idx].classList.contains('full')){
+        idx--
+    }
+
+    smallCups.forEach((cup, idx2) =>{
+        if(idx2 <= idx){
+            cup.classList.add('full')
+        }else{
+            cup.classList.remove('full')
+        }
+    })
+updateBigCup()
+}
+
+function updateBigCup(){
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+    
+    if(fullCups === 0){
+        percentage.style.visibility='hidden'
+        percentage.style.height=0
+    }else{
+        percentage.style.visibility='visible'
+        percentage.style.height=`${fullCups/totalCups*330}px`
+        percentage.innerHTML=`${fullCups/totalCups*100}%`
+    }
+
+    if(fullCups===totalCups){
+        remained.style.visibility='hidden'
+        remained.style.height=0
+    }else{
+        remained.style.visibility='visible'
+        liters.innerText=`${2-(250*fullCups/1000)}`
+    }
+
+}
